bf2e8e2bf2bcdbb9526adc8725c425736df6f166
Sherri Sullivan <sherril.sulli@gmail.com>
2023-11-10 17:03:55 -0500

    fixed gamelog functions, arrays, and tables to match new API endpoints -still have to access new API

--- scripts/RW_gamelog.js
+++ scripts/RW_gamelog.js
@@ -1,114 +1,110 @@
-/**** Gamelog Scripts for 2021 Red Wings site ****/
-/*** pulls info from getGames.js ***/
+/** ** Gamelog Scripts for 2023/2024 Red Wings site ****/
+/** * pulls info from getGames.js ***/
+import { games } from './RW_getGames.js'
 
-window.addEventListener('load', getGames(processGames));
+window.addEventListener('load', processGames)
 
-//assign values based on screen width (full name/abbreviation)
-let w = window.innerWidth;
+// assign values based on screen width (full name/abbreviation)
+let w = window.innerWidth
 
-window.addEventListener('resize',  () => {
-	w = window.innerWidth;
-	processGames();
-});
+window.addEventListener('resize', () => {
+  w = window.innerWidth
+  processGames()
+})
 
 // create rows and cells from games array created by getGames.js
-function processGames() {
+function processGames () {
+  var table = document.querySelector('#currentSeason')
 
-	var table = document.querySelector("#currentSeason");
+  // create a row for each game
+  for (let i = 0, l = games.length; i < l; i++) {
+    const row = document.createElement('tr')
+    row.setAttribute('id', games[i].gameDate)
 
-	// create a row for each game
-	for ( let i = 0, l = games.length; i < l; i++ ) {
-		let row = document.createElement("tr");
-		row.setAttribute("id",games[i].gameDate);
+    if (games[i].gameNo === 'Pre') {
+      continue
+    }
 
-		if (games[i].gameNo === 'Pre') {
-			row.classList.add('pre');
-		}
+    // if opponenet is TOR set row class to 'tor'
+    if (games[i].homeTeamAbbr === 'TOR' || games[i].awayTeamAbbr === 'TOR') {
+      row.classList.add('tor')
+    }
 
-		// if opponenet is TOR set row class to 'tor'
-		if (games[i].homeTeamAbbr === "TOR" || games[i].awayTeamAbbr === "TOR") {
-			row.classList.add('tor');
-		}
-		table.appendChild(row);
+    // create a table cell for game number
+    const gameNo = document.createElement('td')
+    gameNo.innerHTML = games[i].gameNo
+    row.appendChild(gameNo)
 
-		// create a table cell for game number
-		let gameNo = document.createElement("td");
-		gameNo.innerHTML = games[i].gameNo;
-		row.appendChild(gameNo);
+    // create a table cell for date
+    const date = document.createElement('td')
+    date.innerHTML = games[i].gameDate
+    row.appendChild(date)
 
-		// create a table cell for date
-		let date = document.createElement("td");
-		date.innerHTML = games[i].gameDate;
-		row.appendChild(date);
+    // create a table cell for home/away
+    const gameAt = document.createElement('td')
+    row.appendChild(gameAt)
 
-		// create a table cell for home/away
-		let gameAt = document.createElement("td");
-		row.appendChild(gameAt);
+    // create a table cell for opponent
+    const opp = document.createElement('td')
+    row.appendChild(opp)
 
-		// create a table cell for opponent
-		let opp = document.createElement("td");
-		row.appendChild(opp);
+    // set away games to '@'
+    if (games[i].homeAway === 'away') {
+      gameAt.innerHTML = '@'
+      opp.innerHTML = games[i].homeTeamAbbr
+    }
 
-		// set away games to '@'
-		if (games[i].homeAway === 'away') { 
-			gameAt.innerHTML = "@";
-			opp.innerHTML = (w > 600) ? games[i].homeTeam : games[i].homeTeamAbbr;
-		}
+    // set home games to 'vs'
+    if (games[i].homeAway === 'home') {
+      gameAt.innerHTML = 'vs'
+      opp.innerHTML = games[i].awayTeamAbbr
+    }
 
-		// set home games to 'vs'
-		if (games[i].homeAway === 'home') {
-			gameAt.innerHTML = "vs";
-			opp.innerHTML = (w > 600) ? games[i].awayTeam : games[i].awayTeamAbbr;
-		}
+    // create a table cell for score
+    const score = document.createElement('td')
+    row.appendChild(score)
 
-		// create a table cell for score
-		let score = document.createElement("td");
-		row.appendChild(score);
+    // create a table cell for DET record
+    const record = document.createElement('td')
+    row.appendChild(record)
 
-		// create a table cell for DET record
-		let record = document.createElement("td");
-		row.appendChild(record);
+    // create a table cell for DET points
+    const points = document.createElement('td')
+    row.appendChild(points)
 
-		// create a table cell for DET points
-		let points = document.createElement("td");
-		row.appendChild(points);
+    // display according to past or future game and screen size
+    // if game is finished
+    if (games[i].finished) {
+      // remove opacity and make background white
+      row.style.removeProperty('opacity')
+      row.style.background = 'white'
 
-		// display according to past or future game and screen size
-		// if game is finished
-		if ( games[i].finished ) {
-			// remove opacity and make background white
-			// unless it's a preseason game
-			if (games[i].gameNo !== 'Pre') {
-				row.style.removeProperty("opacity");
-				row.style.background = "white";
-			}
+      // and on a small screen
+      if (w < 900) {
+        // display only scores (DET first)
+        score.innerHTML = games[i].detScore + ' | ' + games[i].oppScore
+        // on a large screen
+      } else {
+        // display DET: and score
+        score.innerHTML = 'DET: ' + games[i].detScore + ' | '
+        // if home game display away abbr else home abbr
+        score.innerHTML += games[i].homeAway === 'home' ? games[i].awayTeamAbbr : games[i].homeTeamAbbr
+        // and score
+        score.innerHTML += ': ' + games[i].oppScore
+      }
 
-			// and on a small screen
-			if (w < 900) {
-				// display only scores (DET first)
-				score.innerHTML = games[i].detScore + ' | ' + games[i].oppScore;
-				// on a large screen
-			} else {
-				// display DET: and score
-				score.innerHTML = 'DET: ' + games[i].detScore + ' | ';
-				// if home game display away abbr else home abbr 
-				score.innerHTML += games[i].homeAway === 'home'  ? games[i].awayTeamAbbr : games[i].homeTeamAbbr;  
-				// and score
-				score.innerHTML += ': ' + games[i].oppScore;
-			}
+      // set record and points to record and points
+      record.innerHTML = games[i].detRecord
+      points.innerHTML = games[i].detPoints
 
-			// set record and points to record and points
-			record.innerHTML = games[i].detRecord;
-			points.innerHTML = games[i].detPoints;
+      // add 'win' 'loss' 'otl' to score and row classes
+      score.classList.add(games[i].gameResult)
+      row.classList.add(games[i].gameResult)
 
-			// add 'win' 'loss' 'otl' to score and row classes
-			score.classList.add(games[i].gameResult);
-			row.classList.add(games[i].gameResult);
-
-		// if game hasn't happened yet set opacity to two-thirds
-		} else {
-			row.style.opacity = 0.67;
-		}
-	
-	}
+      // if game hasn't happened yet set opacity to two-thirds
+    } else {
+      row.style.opacity = 0.67
+    }
+    table.appendChild(row)
+  }
 }
--- scripts/RW_getGames.js
+++ scripts/RW_getGames.js
@@ -1,140 +1,145 @@
-/**** getGames Scripts for 2023/24 Red Wings site ****/
-/*** feeds calendar and gamelog ***/
+/** ** getGames Scripts for 2023/24 Red Wings site ****/
+/** * feeds calendar and gamelog ***/
 
-//create an array of game info to be accessed by gameLog and calendar
-const games = [];
+import gameData from '../files/gameData.js'
 
-//get today's date - format to match gameDate from api
-const d = new Date();
-const yyyy = d.getFullYear();
-let m = d.getMonth();
-let mm = m + 1 < 10 ? '0' + (m + 1) : (m + 1);
-let dd = d.getDate();
-dd = dd < 10 ? '0' + dd : dd;
-const dayID = `${yyyy}-${mm}-${dd}`;
+// create an array of game info to be accessed by gameLog and calendar
+const games = []
 
+// get today's date - format to match gameDate from api
+const d = new Date()
+const yyyy = d.getFullYear()
+const m = d.getMonth()
+const mm = m + 1 < 10 ? '0' + (m + 1) : (m + 1)
+let dd = d.getDate()
+dd = dd < 10 ? '0' + dd : dd
+const dayID = `${yyyy}-${mm}-${dd}`
 
-//assign team abbreviations
-function teamAbbr(id) {
-	switch(id) {
-		case 1: return 'NJD';
-		case 2: return 'NYI';
-		case 3: return 'NYR';
-		case 4: return 'PHI';
-		case 5: return 'PIT';
-		case 6: return 'BOS';
-		case 7: return 'BUF';
-		case 8: return 'MTL';
-		case 9: return 'OTT';
-		case 10: return 'TOR';
-		case 12: return 'CAR';
-		case 13: return 'FLA';
-		case 14: return 'TBL';
-		case 15: return 'WSH';
-		case 16: return 'CHI';
-		case 17: return 'DET';
-		case 18: return 'NSH';
-		case 19: return 'STL';
-		case 20: return 'CGY';
-		case 21: return 'COL';
-		case 22: return 'EDM';
-		case 23: return 'VAN';
-		case 24: return 'ANA';
-		case 25: return 'DAL';
-		case 26: return 'LAK';
-		case 28: return 'SJS';
-		case 29: return 'CBJ';
-		case 30: return 'MIN';
-		case 52: return 'WPG';
-		case 53: return 'ARI';
-		case 54: return 'VGK';
-		case 55: return 'SEA';
-	}
+// assign team abbreviations
+function teamAbbr (id) {
+  switch (id) {
+    case 1: return 'NJD'
+    case 2: return 'NYI'
+    case 3: return 'NYR'
+    case 4: return 'PHI'
+    case 5: return 'PIT'
+    case 6: return 'BOS'
+    case 7: return 'BUF'
+    case 8: return 'MTL'
+    case 9: return 'OTT'
+    case 10: return 'TOR'
+    case 12: return 'CAR'
+    case 13: return 'FLA'
+    case 14: return 'TBL'
+    case 15: return 'WSH'
+    case 16: return 'CHI'
+    case 17: return 'DET'
+    case 18: return 'NSH'
+    case 19: return 'STL'
+    case 20: return 'CGY'
+    case 21: return 'COL'
+    case 22: return 'EDM'
+    case 23: return 'VAN'
+    case 24: return 'ANA'
+    case 25: return 'DAL'
+    case 26: return 'LAK'
+    case 28: return 'SJS'
+    case 29: return 'CBJ'
+    case 30: return 'MIN'
+    case 52: return 'WPG'
+    case 53: return 'ARI'
+    case 54: return 'VGK'
+    case 55: return 'SEA'
+  }
 }
 
+// function getGames(callback) {
+//   // fetch('https://statsapi.web.nhl.com/api/v1/schedule?teamId=17&season=20232024')
+//   fetch('https://api-web.nhle.com/v1/club-schedule-season/det/20232024')
+//     .then(response => console.log(response))
+//     .catch(error => console.error(error))
+//     .then((results) => results.json())
+//     .then((data) => {
 
-function getGames(callback) {
-	fetch('https://statsapi.web.nhl.com/api/v1/schedule?teamId=17&season=20232024')
-	.then((results) => results.json())
-	.then((data) => {
-		let gameNo = 0;
-		let prevOtl = 0;
-		let gameResult = '';
-		let detScore = 0;
-		let oppScore = 0;
-		let detWins = 0;
-		let detLoss = 0;
-		let detOtl = 0;
-		let detRecord = '';
-		let detPoints;
-		for (let i = 0, l = data.dates.length; i < l; i++) {
-			gameNo++;
-			let gamePk = data.dates[i].games[0].gamePk;
-			let finished = data.dates[i].games[0].status.detailedState === 'Final' ? true : false;
-			let gameDate = data.dates[i].date;
-			let date = new Date(gameDate);
+function buildGames () {
+  // initialize variables
+  let gameNo = 0
+  let gameResult = ''
+  let detScore = 0
+  let oppScore = 0
+  let detWins = 0
+  let detLoss = 0
+  let detOtl = 0
+  let detRecord = ''
+  let detPoints
 
-			let homeTeam = data.dates[i].games[0].teams.home.team.name;
-			let homeTeamID = data.dates[i].games[0].teams.home.team.id;
-			let homeTeamAbbr = teamAbbr(homeTeamID);
-			let homeScore = data.dates[i].games[0].teams.home.score;
+  for (let i = 0, l = gameData.games.length; i < l; i++) {
+    gameNo++
+    const finished = gameData.games[i].gameState === 'FINAL' || gameData.games[i].gameState === 'OFF'
+    const gameDate = gameData.games[i].gameDate
+    const date = new Date(gameDate)
+    const homeTeamID = gameData.games[i].homeTeam.id
+    const homeTeamAbbr = teamAbbr(homeTeamID)
+    const homeScore = gameData.games[i].homeTeam.score
+    const awayTeamID = gameData.games[i].awayTeam.id
+    const awayTeamAbbr = teamAbbr(awayTeamID)
+    const awayScore = gameData.games[i].awayTeam.score
+    let homeAway = 'away'
 
-			let awayTeam = data.dates[i].games[0].teams.away.team.name;
-			let awayTeamID = data.dates[i].games[0].teams.away.team.id;
-			let awayTeamAbbr = teamAbbr(awayTeamID);
-			let awayScore = data.dates[i].games[0].teams.away.score;
+    if (homeTeamAbbr === 'DET') {
+      homeAway = 'home'
+      detScore = homeScore
+      oppScore = awayScore
+    } else if (awayTeamAbbr === 'DET') {
+      detScore = awayScore
+      oppScore = homeScore
+    }
 
-			let homeAway = 'away';
+    if (finished) {
+      const wentOT = gameData.games[i].gameOutcome.lastPeriodType === 'OT'
+      gameResult = detScore > oppScore ? 'win' : 'loss'
 
-			if ( homeTeam === 'Detroit Red Wings') {
-				homeAway = 'home';
-				detWins = data.dates[i].games[0].teams.home.leagueRecord.wins;
-				detLoss = data.dates[i].games[0].teams.home.leagueRecord.losses;
-				detOtl = data.dates[i].games[0].teams.home.leagueRecord.ot;
-				detScore = homeScore;
-				oppScore = awayScore;
-			} else if ( awayTeam === 'Detroit Red Wings') {
-				detWins = data.dates[i].games[0].teams.away.leagueRecord.wins;
-				detLoss = data.dates[i].games[0].teams.away.leagueRecord.losses;
-				detOtl = data.dates[i].games[0].teams.away.leagueRecord.ot;
-				detScore = awayScore;
-				oppScore = homeScore;
-			}
+      if (wentOT && gameResult === 'loss') {
+        gameResult = 'otl'
+      }
 
-			if ( finished ) { 
-				gameResult = detScore > oppScore ? 'win' : 'loss';
-				if ( detOtl > prevOtl ) {
-					gameResult = 'otl';
-				}
-				prevOtl = detOtl;
-				detRecord = `${detWins} - ${detLoss} - ${detOtl}`;
-				detPoints = (detWins * 2) + detOtl;
-			}
-			
-			if ( data.dates[i].games[0].gameType === "PR") {
-				detRecord = '';
-				detPoints = '';
-				gameNo = 0;
-			}
+      if (gameResult === 'win') {
+        detWins++
+      } else if (gameResult === 'loss') {
+        detLoss++
+      } else if (gameResult === 'otl') {
+        detOtl++
+      } else {
+        console.log('error processing record')
+      }
 
-			games.push({
-				gameNo: (gameNo !== 0) ? gameNo : 'Pre',
-				finished: finished,
-				gameDate: gameDate,
-				awayTeam: awayTeam,
-				awayTeamAbbr: awayTeamAbbr,
-				homeTeam: homeTeam,
-				homeTeamAbbr: homeTeamAbbr,
-				awayScore: awayScore,
-				homeScore: homeScore,
-				detScore: detScore,
-				oppScore: oppScore,
-				detRecord: detRecord,
-				detPoints: detPoints,
-				gameResult: gameResult,
-				homeAway: homeAway
-			});
-		}
-		callback();
-	});
+      detRecord = `${detWins} - ${detLoss} - ${detOtl}`
+      detPoints = (detWins * 2) + detOtl
+    }
+
+    if (gameData.games[i].gameType === 1) {
+      detRecord = ''
+      detPoints = ''
+      gameNo = 0
+    }
+
+    games.push({
+      gameNo: (gameNo !== 0) ? gameNo : 'Pre',
+      finished: finished,
+      gameDate: gameDate,
+      awayTeamAbbr: awayTeamAbbr,
+      homeTeamAbbr: homeTeamAbbr,
+      awayScore: awayScore,
+      homeScore: homeScore,
+      detScore: detScore,
+      oppScore: oppScore,
+      detRecord: detRecord,
+      detPoints: detPoints,
+      gameResult: gameResult,
+      homeAway: homeAway
+    })
+  }
 }
+
+buildGames()
+export { games }
